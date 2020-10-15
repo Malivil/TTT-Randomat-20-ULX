8799ab9aaabbf89c44db8e8b08f7366973de92b0
Malivil <mlebson@outlook.com>
2020-10-15 15:44:01 -0400

    Moved distribution scripts to their own repo

--- UpdateRandomatULX.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p changes="Enter Changes: "
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe"  update -addon "TTT-Randomat-20-ULX.gma" -id "2096758509" -changes "%changes%"
-pause
